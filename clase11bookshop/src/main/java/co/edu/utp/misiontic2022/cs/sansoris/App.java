b11956cd760bad45dddbd1d9d66fcab787643fb9
sansoris <sansoris@hotmail.com>
2021-07-27 15:58:17 -0500

    Creacion proyecto

---
+++ clase11bookshop/src/main/java/co/edu/utp/misiontic2022/cs/sansoris/App.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.cs.sansoris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
